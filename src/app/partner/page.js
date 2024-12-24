e6e1de64c2b1b1188d1f1c81756e20f6a4fb835c
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-12-24 23:03:51 +0100

    Add files via upload

--- src/app/partner/page.js
+++ src/app/partner/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from 'react';
 import { FaFacebook, FaYoutube, FaTelegram, FaGoogle, FaWhatsapp, FaInstagram } from 'react-icons/fa';
@@ -45,6 +45,15 @@ const Page = () => {
       setLoading(false);
       alert("Success");
       console.log('Partnership successful:', response.data);
+
+      // Reset form data after submission
+      setFormData({
+        name: '',
+        email: '',
+        partnership: '',
+        phoneNumber: ''
+      });
+      setSelectedPartnership('');
     } catch (error) {
       setLoading(false);
       console.error('Error during Partnership:', error);
@@ -83,9 +92,9 @@ const Page = () => {
                 <li><a className='ml-1 rounded-md bg-[#B41000]' href="/">Home</a></li>
                 <li><a className='ml-1 rounded-md bg-[#B41000]' href="/flamezone">Flamezone</a></li>
                 <li><a className='ml-1 rounded-md bg-[#B41000]' href="https://wa.me/+2349032865677">Contact</a></li>
-                <li><a className='ml-1 rounded-md bg-[#B41000]' onClick={()=>document.getElementById('my_modal_1').showModal()}>Media</a></li>
+                <li><a className='ml-1 rounded-md bg-[#B41000]' onClick={() => document.getElementById('my_modal_1').showModal()}>Media</a></li>
                 <li><a className='border-b-[2px] border-[#B41000]' href='/partner'>Become a Partner</a></li>
-                <li><a href='/testify' className='ml-1 rounded-md bg-[#B41000]' >iTestify</a></li>
+                <li><a href='/testify' className='ml-1 rounded-md bg-[#B41000]'>iTestify</a></li>
               </ul>
             </div>
           </div>
@@ -97,39 +106,38 @@ const Page = () => {
             <li><a className="hover:bg-[#B41000]" href="/">Home</a></li>
             <li><a className="hover:bg-[#B41000]" href="/flamezone">Flamezone</a></li>
             <li><a className="hover:bg-[#B41000]" href="https://wa.me/+2349032865677">Contact</a></li>
-            <li><a className="hover:bg-[#B41000]" onClick={()=>document.getElementById('my_modal_1').showModal()}>Media</a></li>
+            <li><a className="hover:bg-[#B41000]" onClick={() => document.getElementById('my_modal_1').showModal()}>Media</a></li>
             <li><a className="hover:bg-[#B41000]" href='/partner'>Become a Partner</a></li>
-            <li><a href='/testify' className='hover:bg-[#B41000]' >iTestify</a></li>
+            <li><a href='/testify' className='hover:bg-[#B41000]'>iTestify</a></li>
           </ul>
         </div>
       </div>
       <div>
-      <dialog id="my_modal_1" className="modal">
-    <div className="modal-box">
-    <div className="flex justify-between w-full">
-    <div className="card cursor-pointer card-compact mt-2 shadow-xl">
-  <a href='https://www.instagram.com/thehofglobalmin/' className="card-body">
-    <GrGallery size={42} color="#B41000" />
-  </a>
-</div>
-<div className="card card-compact cursor-pointer mt-2 shadow-xl">
-  <a href='https://t.me/thehofglobal' className="card-body">
-   <MdAudiotrack size={42} color="#B41000" />
-  </a>
-</div>
-<div className="card card-compact cursor-pointer mt-2 shadow-xl">
-  <a href="https://www.youtube.com/@thehofglobalmin" className="card-body"> 
-   <MdOutlineSlowMotionVideo size={42} color="#B41000" />
-  </a>
-</div>
-    </div>
-      <form method="dialog" className="modal-backdrop">
-        {/* if there is a button in form, it will close the modal */}
-        <button className="btn bg-[#B41000]">Close</button>
-      </form>
-  </div>
-</dialog>
-    </div>
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box">
+            <div className="flex justify-between w-full">
+              <div className="card cursor-pointer card-compact mt-2 shadow-xl">
+                <a href='https://www.instagram.com/thehofglobalmin/' className="card-body">
+                  <GrGallery size={42} color="#B41000" />
+                </a>
+              </div>
+              <div className="card card-compact cursor-pointer mt-2 shadow-xl">
+                <a href='https://t.me/thehofglobal' className="card-body">
+                  <MdAudiotrack size={42} color="#B41000" />
+                </a>
+              </div>
+              <div className="card card-compact cursor-pointer mt-2 shadow-xl">
+                <a href="https://www.youtube.com/@thehofglobalmin" className="card-body">
+                  <MdOutlineSlowMotionVideo size={42} color="#B41000" />
+                </a>
+              </div>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button className="btn bg-[#B41000]">Close</button>
+            </form>
+          </div>
+        </dialog>
+      </div>
       <div className="lg:hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:ml-16 lg:text-left">
@@ -214,57 +222,26 @@ const Page = () => {
                   <span className="label-text">Phone Number</span>
                 </label>
                 <input
-                  type="number"
+                  type="text"
                   name="phoneNumber"
                   value={formData.phoneNumber}
                   onChange={handleChange}
+                  placeholder="Phone Number"
                   className="input input-bordered"
                   required
                 />
               </div>
               <div className="form-control mt-6">
-                <button type="submit" className="btn bg-[#B41000]">
-                  {loading ? (
-                    <span className="loading loading-ring loading-lg"></span>
-                  ) : (<span>Submit</span>)}
+                <button className={`btn ${loading ? 'loading' : 'bg-[#B41000]'}`} type="submit">
+                  Submit
                 </button>
               </div>
             </form>
           </div>
         </div>
       </div>
-      <footer className="footer footer-center bg-[#B41000] text-primary-content p-2 md:p-10">
-        <aside>
-          <Image src={Logo} width={50} height={50} />
-          <p className="font-bold text-white">
-            HOF Global
-            <br />
-          </p>
-          <p className="text-white">Copyright © 2024 - All right reserved</p>
-    <div className="text-white flex"><FaGoogle className="mr-1 pt-[2px]" size={16} /><p className="text-[12px] ">thehofglobal@gmail.com</p></div>
-  </aside>
-  <nav>
-    <div className="grid grid-flow-col gap-1">
-      <a href='facebook.com/thehofglobalmin'>
-        <FaFacebook size={24} color="#fff" />
-      </a>
-      <a href='https://wa.me/+2349032865677'>
-        <FaWhatsapp size={24} color="#fff" />
-      </a>
-      <a href='https://t.me/thehofglobal'>
-        <FaTelegram size={24} color="#fff" />
-      </a>
-      <a href='https://www.instagram.com/thehofglobalmin/ '>
-        <FaInstagram size={24} color="#fff" />
-      </a>
-      <a href='https://www.youtube.com/@thehofglobalmin'>
-        <FaYoutube size={24} color="#fff" />
-      </a> 
-    </div>
-  </nav>
-</footer>
     </div>
   );
-}
+};
 
 export default Page;
